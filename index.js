c0496ab2c8decb1d0c69edb765c1fcf93b27ffd3
Nabin Pokharel <pokhrelnabin17@gmail.com>
2025-04-27 11:12:55 +0545

    sever side all set

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ res.send("HII")
 })
 const io= new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin:["http://localhost:5173", "http://192.168.1.65:5173"],
       methods: ["GET", "POST"],
       credentials: true
     }
